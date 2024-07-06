6ed601d7802071d176447755a0ad08d2c0e01d46
Artur Gubaidullin <lapteuh@gmail.com>
2024-07-06 10:19:36 +0000

    Add project

---
+++ contracts/order-accepted/src/lib/order-accepted.spec.ts
@@ -0,0 +1,7 @@
+import { orderAccepted } from './order-accepted';
+
+describe('orderAccepted', () => {
+  it('should work', () => {
+    expect(orderAccepted()).toEqual('order-accepted');
+  });
+});
---
+++ contracts/order-accepted/src/lib/order-accepted.ts
@@ -0,0 +1,3 @@
+export function orderAccepted(): string {
+  return 'order-accepted';
+}
