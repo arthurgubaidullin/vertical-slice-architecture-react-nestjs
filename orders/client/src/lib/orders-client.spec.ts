836d8aedc5a0299a62c7fafbc8b8de63cf430d20
Artur Gubaidullin <lapteuh@gmail.com>
2024-07-06 12:30:53 +0000

    Wip

--- orders/client/src/lib/orders-client.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ordersClient } from './orders-client';
-
-describe('ordersClient', () => {
-  it('should work', () => {
-    expect(ordersClient()).toEqual('orders-client');
-  });
-});
--- orders/client/src/lib/orders-client.ts
+++ orders/client/src/lib/orders-client.ts
@@ -1,3 +1,13 @@
-export function ordersClient(): string {
-  return 'orders-client';
-}
+import { NewOrderForm } from '@org/new-order-form';
+import { observable } from 'mobx';
+
+export const create = () => ({
+  add: (newOrderForm: NewOrderForm) => {
+    console.log('New order form received.', newOrderForm);
+  },
+  list: () => {
+    const orders$ = observable.box(null);
+
+    return { get: () => orders$.get() };
+  },
+});
