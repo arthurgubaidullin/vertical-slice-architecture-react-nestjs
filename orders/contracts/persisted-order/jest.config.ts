5175604ad3e16d7e4f6cd52a11d521b26cf341be
Artur Gubaidullin <lapteuh@gmail.com>
2024-07-06 15:32:41 +0000

    Move project

--- orders/contracts/persisted-order/jest.config.ts
+++ orders/contracts/persisted-order/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
   displayName: 'persisted-order',
-  preset: '../../jest.preset.js',
+  preset: '../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/orders/persisted-order',
+  coverageDirectory: '../../../coverage/orders/contracts/persisted-order',
 };
