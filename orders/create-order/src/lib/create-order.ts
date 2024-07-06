c4e815f5c2b62b635fb70434b428d638cdcedb48
Artur Gubaidullin <lapteuh@gmail.com>
2024-07-06 15:57:56 +0000

    Add function

--- orders/create-order/src/lib/create-order.ts
+++ orders/create-order/src/lib/create-order.ts
@@ -1,5 +1,24 @@
 import { NewOrderForm } from '@org/new-order-form';
+import { PersistedOrder } from '@org/persisted-order';
+import * as E from 'fp-ts/Either';
+import { pipe } from 'fp-ts/function';
+import { failure } from 'io-ts/PathReporter';
 
-export function createOrder(form: NewOrderForm) {
-  return form;
+class NewOrderFormIsInvalid {
+  readonly _tag = 'InvalidNewOrderForm' as const;
+
+  constructor(
+    public readonly form: unknown,
+    public readonly errors: string[]
+  ) {}
 }
+
+export const createOrder = (
+  newOrderForm: unknown
+): E.Either<NewOrderFormIsInvalid, PersistedOrder> =>
+  pipe(
+    newOrderForm,
+    NewOrderForm.decode,
+    E.mapLeft(failure),
+    E.mapLeft((errors) => new NewOrderFormIsInvalid(newOrderForm, errors))
+  );
