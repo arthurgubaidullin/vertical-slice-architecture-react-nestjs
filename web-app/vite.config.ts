984f4983f6fbc89991406e252eeaffb3a4552b6a
Artur Gubaidullin <lapteuh@gmail.com>
2024-07-06 16:52:12 +0000

    Fix vite config

--- web-app/vite.config.ts
+++ web-app/vite.config.ts
@@ -10,6 +10,12 @@ export default defineConfig({
   server: {
     port: 4200,
     host: '0.0.0.0',
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+    },
   },
 
   preview: {
