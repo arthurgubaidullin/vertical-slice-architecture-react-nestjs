09252b4fc28a46ce97b0b73d833f943d199a0971
Artur Gubaidullin <lapteuh@gmail.com>
2024-07-06 14:08:25 +0000

    Add function

--- orders/client/src/lib/orders-store.ts
+++ orders/client/src/lib/orders-store.ts
@@ -1,45 +1,24 @@
 import * as RD from '@devexperts/remote-data-ts';
-import * as UUID from '@org/uuid-v4';
-import { action, observable, onBecomeObserved, runInAction } from 'mobx';
+import { NewOrderForm } from '@org/new-order-form';
+import { pipe } from 'fp-ts/function';
+import { action, observable } from 'mobx';
 import { Order } from './order';
-
-const orders: ReadonlyArray<Order> = [
-  {
-    id: UUID.randomUUID(),
-    goods: 'Awesomeness',
-    quantity: 1,
-    total: 1.05,
-  },
-  {
-    id: UUID.randomUUID(),
-    goods: 'Awesomeness',
-    quantity: 10,
-    total: 100,
-  },
-  {
-    id: UUID.randomUUID(),
-    goods: 'Awesomeness',
-    quantity: 3,
-    total: 146,
-  },
-];
 
 export const create = () => {
   const orders$ = observable.box<RD.RemoteData<Error, ReadonlyArray<Order>>>(
     RD.initial
   );
 
-  onBecomeObserved(orders$, () => {
-    runInAction(() => orders$.set(RD.pending));
+  const add = action((newOrderForm: NewOrderForm) =>
+    pipe(
+      orders$.get(),
+      RD.map((orders) => [...orders, newOrderForm]),
+      (value) => orders$.set(value)
+    )
+  );
 
-    setTimeout(
-      action(() => {
-        orders$.set(RD.success(orders));
-        console.log('lol', orders);
-      }),
-      3000
-    );
-  });
-
-  return { get: () => orders$.get() };
+  return {
+    get: () => orders$.get(),
+    add,
+  };
 };
