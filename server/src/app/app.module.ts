eb711fe88f3ba5b108f13220353a1477a4a8dab5
Artur Gubaidullin <lapteuh@gmail.com>
2024-07-06 14:56:00 +0000

    Add nest module to server

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NotificationsModule } from '@org/notifications-nest-module';
+import { SalesServerModule } from '@org/sales-server';
 
 @Module({
-  imports: [NotificationsModule],
+  imports: [NotificationsModule, SalesServerModule],
   controllers: [AppController],
   providers: [AppService],
 })
