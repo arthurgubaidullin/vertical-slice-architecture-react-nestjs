f3157e0c6d032e00079c40edf76888db1e6a9f39
Artur Gubaidullin <lapteuh@gmail.com>
2024-07-06 18:09:44 +0000

    Add event emitter to nest server

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
-
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { NotificationsModule } from '@org/notifications-nest-module';
+import { OrdersModule } from '@org/orders-nest-module';
+import { SalesModule } from '@org/sales-nest-module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { NotificationsModule } from '@org/notifications-nest-module';
-import { SalesModule } from '@org/sales-nest-module';
-import { OrdersModule } from '@org/orders-nest-module';
 
 @Module({
-  imports: [NotificationsModule, SalesModule, OrdersModule],
+  imports: [
+    EventEmitterModule.forRoot(),
+    NotificationsModule,
+    SalesModule,
+    OrdersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
