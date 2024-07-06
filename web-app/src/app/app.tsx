f0b86188808dc5517047092bea29de2e5f006dad
Artur Gubaidullin <lapteuh@gmail.com>
2024-07-06 08:28:08 +0000

    Wip

--- web-app/src/app/app.tsx
+++ web-app/src/app/app.tsx
@@ -1,7 +1,50 @@
 function NewOrderForm() {
   return (
     <div>
-      <h1>New Order Form</h1>
+      <h1 className="text-2xl">New Order Form</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <label className="form-control w-full">
+          <div className="label">
+            <span className="label-text">What do you want to buy?</span>
+            <span className="label-text-alt">Required</span>
+          </div>
+          <input
+            type="text"
+            placeholder="Awesomeness"
+            className="input input-bordered w-full"
+            required
+          />
+        </label>
+        <label className="form-control w-full">
+          <div className="label">
+            <span className="label-text">How much do you want to buy?</span>
+            <span className="label-text-alt">Required</span>
+          </div>
+          <input
+            type="number"
+            placeholder="9999"
+            className="input input-bordered w-full"
+            required
+          />
+        </label>
+
+        <div>
+          <div className="form-control">
+            <label className="label cursor-pointer">
+              <span className="label-text">
+                I accept all the rules of the service
+              </span>
+              <input type="checkbox" className="checkbox" required />
+            </label>
+          </div>
+        </div>
+
+        <input className="btn btn-primary" type="submit" />
+      </form>
     </div>
   );
 }
