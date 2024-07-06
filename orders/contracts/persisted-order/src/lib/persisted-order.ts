b41f926c54f3f8cefbbe879c418c5ceb390d4bfc
Artur Gubaidullin <lapteuh@gmail.com>
2024-07-06 15:57:45 +0000

    Add type

--- orders/contracts/persisted-order/src/lib/persisted-order.ts
+++ orders/contracts/persisted-order/src/lib/persisted-order.ts
@@ -1,3 +1,13 @@
-export function persistedOrder(): string {
-  return 'persisted-order';
-}
+import * as t from 'io-ts';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface PersistedOrder extends t.TypeOf<typeof PersistedOrder> {}
+
+export const PersistedOrder = t.readonly(
+  t.strict({
+    id: t.string,
+    goods: t.string,
+    quantity: t.number,
+    total: t.number,
+  })
+);
