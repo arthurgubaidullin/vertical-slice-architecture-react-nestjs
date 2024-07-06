9724a98c92e3807e9fb14beed84267f2718b3430
Artur Gubaidullin <lapteuh@gmail.com>
2024-07-06 15:45:52 +0000

    Remove unused

--- orders/contracts/persisted-order/src/lib/persisted-order.spec.ts
+++
@@ -1,7 +0,0 @@
-import { persistedOrder } from './persisted-order';
-
-describe('persistedOrder', () => {
-  it('should work', () => {
-    expect(persistedOrder()).toEqual('persisted-order');
-  });
-});
