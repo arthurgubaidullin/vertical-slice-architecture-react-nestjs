61944d06120f861952892191e66dae434027cda3
Artur Gubaidullin <lapteuh@gmail.com>
2024-07-06 11:33:53 +0000

    Add function

--- sales/client/src/lib/sales-client.spec.ts
+++
@@ -1,7 +0,0 @@
-import { salesClient } from './sales-client';
-
-describe('salesClient', () => {
-  it('should work', () => {
-    expect(salesClient()).toEqual('sales-client');
-  });
-});
--- sales/client/src/lib/sales-client.ts
+++ sales/client/src/lib/sales-client.ts
@@ -1,3 +1,24 @@
-export function salesClient(): string {
-  return 'sales-client';
-}
+import { action, observable } from 'mobx';
+import * as NewOrderForm from '@org/new-order-form';
+import { constVoid, pipe } from 'fp-ts/function';
+import * as O from 'fp-ts/Option';
+
+export const create = (id: string) => {
+  const store = observable.box<NewOrderForm.NewOrderForm>(
+    NewOrderForm.initial(id),
+    { deep: false }
+  );
+
+  const change = action((data: Partial<NewOrderForm.NewOrderForm>): void =>
+    pipe(
+      store.get(),
+      NewOrderForm.change(data),
+      O.fold(constVoid, (a) => store.set(a))
+    )
+  );
+
+  return {
+    get: () => store.get(),
+    change,
+  };
+};
