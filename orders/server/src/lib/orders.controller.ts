c5266f2addb163da288d518aaa5890725018aaed
Artur Gubaidullin <lapteuh@gmail.com>
2024-07-06 16:12:23 +0000

    Wip

--- orders/server/src/lib/orders.controller.ts
+++ orders/server/src/lib/orders.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 
 @Controller('orders-server')
 export class OrdersController {
   constructor(private ordersService: OrdersService) {}
+
+  @Post('orders')
+  add(@Body() form: unknown) {
+    return this.ordersService.add(form);
+  }
 }
--- orders/server/src/lib/orders.service.ts
+++ orders/server/src/lib/orders.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { createOrder } from '@org/create-order';
+import { PersistedOrder } from '@org/persisted-order';
+import { pipe } from 'fp-ts/function';
+import * as E from 'fp-ts/Either';
+
+const db = new Map<string, PersistedOrder>();
 
 @Injectable()
-export class OrdersService {}
+export class OrdersService {
+  public add(form: unknown) {
+    return pipe(
+      createOrder(form),
+      E.fold(
+        (e) => {
+          throw e;
+        },
+        (order) => {
+          if (!db.has(order.id)) {
+            db.set(order.id, order);
+          }
+        }
+      )
+    );
+  }
+}
