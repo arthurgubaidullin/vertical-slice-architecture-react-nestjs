b325e330cebab4129c8e3d6e84a5bf62fbd1aa48
Artur Gubaidullin <lapteuh@gmail.com>
2024-07-07 09:34:46 +0000

    Add controller method

--- notifications/server/src/lib/notifications.controller.ts
+++ notifications/server/src/lib/notifications.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
+import { ExtractedNotification } from '@org/extracted-notification';
 
-@Controller('notifications-nest-module')
+@Controller('notifications')
 export class NotificationsController {
-  constructor(private NotificationsService: NotificationsService) {}
+  constructor(private notificationsService: NotificationsService) {}
+
+  @Get()
+  list(): ReadonlyArray<ExtractedNotification> {
+    return this.notificationsService
+      .list()
+      .map((v) => ExtractedNotification.encode(v));
+  }
 }
