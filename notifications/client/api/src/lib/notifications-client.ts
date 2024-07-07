cf1e50d94c6c7cf285c95f1883aa74965ea3b40f
Artur Gubaidullin <lapteuh@gmail.com>
2024-07-07 09:44:55 +0000

    Add api client

--- notifications/client/api/src/lib/notifications-client.ts
+++ notifications/client/api/src/lib/notifications-client.ts
@@ -1,3 +1,26 @@
-export function notificationsClient(): string {
-  return 'notifications-client';
-}
+import { ExtractedNotificationList } from '@org/extracted-notification';
+import * as E from 'fp-ts/Either';
+import { pipe } from 'fp-ts/function';
+import * as TE from 'fp-ts/TaskEither';
+import { failure } from 'io-ts/PathReporter';
+
+const list = () =>
+  pipe(
+    TE.tryCatch(
+      () => fetch('/api/notifications', { credentials: 'omit' }),
+      E.toError
+    ),
+    TE.chainW((response) => TE.tryCatch(() => response.json(), E.toError)),
+    TE.chainEitherKW((data) =>
+      pipe(
+        data,
+        ExtractedNotificationList.decode,
+        E.mapLeft(failure),
+        E.mapLeft(E.toError)
+      )
+    )
+  );
+
+export const create = () => ({
+  list,
+});
