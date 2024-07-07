5a3571005d4548acf4d5dd3b339d182d23d56ce7
Artur Gubaidullin <lapteuh@gmail.com>
2024-07-07 09:18:31 +0000

    Add type

--- notifications/contracts/persisted-notification/src/lib/persisted-notification.spec.ts
+++
@@ -1,7 +0,0 @@
-import { persistedNotification } from './persisted-notification';
-
-describe('persistedNotification', () => {
-  it('should work', () => {
-    expect(persistedNotification()).toEqual('persisted-notification');
-  });
-});
--- notifications/contracts/persisted-notification/src/lib/persisted-notification.ts
+++ notifications/contracts/persisted-notification/src/lib/persisted-notification.ts
@@ -1,3 +1,22 @@
-export function persistedNotification(): string {
-  return 'persisted-notification';
-}
+import * as t from 'io-ts';
+
+const Order = t.readonly(
+  t.strict({
+    id: t.string,
+    goods: t.string,
+    total: t.number,
+  })
+);
+
+export const OrderCreated = t.readonly(
+  t.strict({
+    type: t.literal('order.created'),
+    order: Order,
+  })
+);
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface PersistedNotification
+  extends t.TypeOf<typeof PersistedNotification> {}
+
+export const PersistedNotification = OrderCreated;
