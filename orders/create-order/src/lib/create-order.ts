08786e77bcfd7eb32654e50e804ba4f17f5a754b
Artur Gubaidullin <lapteuh@gmail.com>
2024-07-07 12:28:39 +0000

    Fix function

--- orders/create-order/src/lib/create-order.ts
+++ orders/create-order/src/lib/create-order.ts
@@ -1,5 +1,6 @@
 import { NewOrderForm } from '@org/new-order-form';
 import { PersistedOrder } from '@org/persisted-order';
+import * as Order from '@org/order';
 import * as E from 'fp-ts/Either';
 import { pipe } from 'fp-ts/function';
 import { failure } from 'io-ts/PathReporter';
@@ -15,10 +16,12 @@ class NewOrderFormIsInvalid {
 
 export const createOrder = (
   newOrderForm: unknown
-): E.Either<NewOrderFormIsInvalid, PersistedOrder> =>
+): E.Either<NewOrderFormIsInvalid | Error, PersistedOrder> =>
   pipe(
     newOrderForm,
     NewOrderForm.decode,
     E.mapLeft(failure),
-    E.mapLeft((errors) => new NewOrderFormIsInvalid(newOrderForm, errors))
+    E.mapLeft((errors) => new NewOrderFormIsInvalid(newOrderForm, errors)),
+    E.chainW((newOrderForm) => Order.create(newOrderForm)),
+    E.map((order) => Order.toJSON(order))
   );
