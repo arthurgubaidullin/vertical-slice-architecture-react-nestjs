b3f451f8244704507110ef55f6ea26f9bfda6695
Artur Gubaidullin <lapteuh@gmail.com>
2024-07-07 09:27:03 +0000

    Add repository

--- notifications/server/src/lib/notifications.repository.ts
+++ notifications/server/src/lib/notifications.repository.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { PersistedNotification } from '@org/persisted-notification';
+
+const db = new Map<string, PersistedNotification>();
 
 @Injectable()
-export class NotificationsRepository {}
+export class NotificationsRepository {
+  add(notification: PersistedNotification) {
+    if (!db.has(notification.id)) {
+      db.set(notification.id, notification);
+    }
+  }
+
+  list(): ReadonlyArray<PersistedNotification> {
+    return Array.from(db.values());
+  }
+}
