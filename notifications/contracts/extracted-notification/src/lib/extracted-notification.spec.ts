3687fa408e6dad461265122d27d563ff12452ef5
Artur Gubaidullin <lapteuh@gmail.com>
2024-07-07 09:20:00 +0000

    Add type

--- notifications/contracts/extracted-notification/src/lib/extracted-notification.spec.ts
+++
@@ -1,7 +0,0 @@
-import { extractedNotification } from './extracted-notification';
-
-describe('extractedNotification', () => {
-  it('should work', () => {
-    expect(extractedNotification()).toEqual('extracted-notification');
-  });
-});
--- notifications/contracts/extracted-notification/src/lib/extracted-notification.ts
+++ notifications/contracts/extracted-notification/src/lib/extracted-notification.ts
@@ -1,3 +1,21 @@
-export function extractedNotification(): string {
-  return 'extracted-notification';
-}
+import * as t from 'io-ts';
+
+const Order = t.readonly(
+  t.strict({
+    id: t.string,
+    goods: t.string,
+  })
+);
+
+export const OrderCreated = t.readonly(
+  t.strict({
+    type: t.literal('order.created'),
+    order: Order,
+  })
+);
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface ExtractedNotification
+  extends t.TypeOf<typeof ExtractedNotification> {}
+
+export const ExtractedNotification = OrderCreated;
