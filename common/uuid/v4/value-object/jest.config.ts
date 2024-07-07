93592e7362869ec5922a4a45e7a11c5c416d1582
Artur Gubaidullin <lapteuh@gmail.com>
2024-07-07 13:20:08 +0000

    Move project

--- common/uuid/v4/value-object/jest.config.ts
+++ common/uuid/v4/value-object/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
   displayName: 'uuid-v4',
-  preset: '../../jest.preset.js',
+  preset: '../../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/common/uuid-v4',
+  coverageDirectory: '../../../../coverage/common/uuid/v4/value-object',
 };
