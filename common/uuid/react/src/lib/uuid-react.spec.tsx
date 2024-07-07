a0d580539b352a8889f4b534ee9e2b196caf6c8b
Artur Gubaidullin <lapteuh@gmail.com>
2024-07-07 13:30:38 +0000

    Add project

---
+++ common/uuid/react/src/lib/uuid-react.spec.tsx
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react';
+
+import { UUIDPreview } from './uuid-react';
+
+describe('UuidReact', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(
+      <UUIDPreview uuid="031ff571-6237-4e94-b88d-874ce72b0a4a" />
+    );
+    expect(baseElement).toBeTruthy();
+  });
+});
---
+++ common/uuid/react/src/lib/uuid-react.tsx
@@ -0,0 +1,9 @@
+function createUUIDPreview(uuid: string) {
+  return `${uuid.slice(0, 4)}...${uuid.slice(-4)}`;
+}
+
+export function UUIDPreview(props: { uuid: string }) {
+  const s = createUUIDPreview(props.uuid);
+
+  return <span>{s}</span>;
+}
